bfe7bda20758043a4ed51b641611e7c0835a281b
Erjon Lamy <erjon.lamy@outlook.fr>
2017-06-14 11:34:39 +0200

    Function onClick added to every button

---
+++ app/src/main/java/com/example/admin/dta_android_calculatrice/CalculLogic.java
@@ -0,0 +1,44 @@
+package com.example.admin.dta_android_calculatrice;
+
+import android.util.Log;
+
+/**
+ * Created by admin on 14/06/2017.
+ */
+
+public class CalculLogic {
+    private int stack[];
+    private int result;
+
+    public void enter(int n){
+        Log.d("CalculLogic", "Coucou je suis " + n);
+    }
+
+    public void enter(){
+
+    }
+
+    public void plus(){
+
+    }
+
+    public void minus(){
+
+    }
+
+    public void divide(){
+
+    }
+
+    public void clear(){
+
+    }
+
+    public void pop(){
+
+    }
+
+    public void swap(){
+
+    }
+}
--- app/src/main/java/com/example/admin/dta_android_calculatrice/MainActivity.java
+++ app/src/main/java/com/example/admin/dta_android_calculatrice/MainActivity.java
@@ -2,12 +2,87 @@ package com.example.admin.dta_android_calculatrice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CalculLogic logic = new CalculLogic();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.button_un:
+                logic.enter(1);
+                break;
+
+            case R.id.button_deux:
+                logic.enter(2);
+                break;
+
+            case R.id.button_trois:
+                logic.enter(3);
+                break;
+
+            case R.id.button_quatre:
+                logic.enter(4);
+                break;
+
+            case R.id.button_cinq:
+                logic.enter(5);
+                break;
+
+            case R.id.button_six:
+                logic.enter(6);
+                break;
+
+            case R.id.button_sept:
+                logic.enter(7);
+                break;
+
+            case R.id.button_huit:
+                logic.enter(8);
+                break;
+
+            case R.id.button_neuf:
+                logic.enter(9);
+                break;
+
+            case R.id.button_plus:
+                logic.plus();
+                break;
+
+            case R.id.button_moins:
+                logic.minus();
+                break;
+
+            case R.id.button_diviser:
+                logic.divide();
+                break;
+
+            case R.id.button_swap:
+                logic.swap();
+                break;
+
+            case R.id.button_clear:
+                logic.clear();
+                break;
+
+            case R.id.button_pop:
+                logic.pop();
+                break;
+
+            case R.id.button_enter:
+                logic.enter();
+                break;
+
+        }
     }
 }
